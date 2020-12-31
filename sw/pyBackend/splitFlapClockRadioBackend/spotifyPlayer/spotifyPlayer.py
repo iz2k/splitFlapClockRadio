a45e0ded23b594ecddbfe08ad3268aeb68d44a40
iz2k <ibon@zalbide.com>
2020-12-31 11:27:33 +0100

    Some more Spotify errors handled

--- sw/pyBackend/splitFlapClockRadioBackend/spotifyPlayer/spotifyPlayer.py
+++ sw/pyBackend/splitFlapClockRadioBackend/spotifyPlayer/spotifyPlayer.py
@@ -25,15 +25,24 @@ class SpotifyPlayer:
 
 	def play(self):
 		output=execute('/home/pi/.local/bin/spotify play')
-		self.currentTrack = output.splitlines()[1].lstrip()
+		if len(output.splitlines())>0:
+			self.currentTrack = output.splitlines()[1].lstrip()
+		else:
+			self.currentTrack = 'N/A'
 		print('[spotify] PLAY: ' + self.currentTrack)
 
 	def next(self):
 		output=execute('/home/pi/.local/bin/spotify next')
-		self.currentTrack = output.splitlines()[1].lstrip()
+		if len(output.splitlines())>0:
+			self.currentTrack = output.splitlines()[1].lstrip()
+		else:
+			self.currentTrack = 'N/A'
 		print('[spotify] NEXT: ' + self.currentTrack)
 
 	def previous(self):
 		output=execute('/home/pi/.local/bin/spotify previous')
-		self.currentTrack = output.splitlines()[1].lstrip()
+		if len(output.splitlines())>0:
+			self.currentTrack = output.splitlines()[1].lstrip()
+		else:
+			self.currentTrack = 'N/A'
 		print('[spotify] PREVIOUS: ' + self.currentTrack)
